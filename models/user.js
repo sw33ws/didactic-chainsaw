f04c68db45c4ef0221f39ac4b46d47aa5679bea5
Seb <gillisl22@outlook.com>
2022-07-13 00:35:43 -0400

    adding in the basic files, and folders, finishing models as well

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    // friends: [{
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'User'
+    // }],
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
