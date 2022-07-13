943247155d3180007345ab9daaf5fc43c05ee888
Seb <gillisl22@outlook.com>
2022-07-13 19:36:40 -0400

    adding and delating user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,6 +28,38 @@ const userController = {
         .catch((err) => res.status(500).json(err))
     },
     // creating user
+    createUser(req, res){
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(400).json(err));
+    },
+    // updating user
+    updateUser(req, res){
+        User.findByIdAndUpdate(
+            { _id: req.params.id}, { $set: req.body }, { runValidators: true, new: true})
+            .then(user => {
+                if (!user) {
+                    res.status(404).json({ message: 'no user found!'});
+                    return;
+                }
+                res.json(user);
+            })
+            .catch(err => res.json(err));
+    },
+    // delating user
+    deleteUser(req, res) {
+        User.findByIdAndRemove(
+            { _id: req.params.id}
+        )
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: 'no user found!'});
+                return;
+            }
+            res.json(user);
+        })
+        .catch(err => res.json(err));
+    }
 }
 
 module.exports = userController;
