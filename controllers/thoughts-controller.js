a83b7ff1e09e6eb6f7a2de1f406d4808d65a0e87
Seb <gillisl22@outlook.com>
2022-07-13 20:24:15 -0400

    thought creation, updating, and delating

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -25,6 +25,49 @@ const thoughtsController = {
         .catch((err) => res.status(500).json(err))
     },
     // creating thought
+    createThought(req, res) {
+        Thoughts.create(req.body)
+        .then((thoughtsdata) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.id}, { $addToSet: { thoughts: thoughtsdata._id}}, { new: true}
+            );
+        })
+        .then((thoughtsdata) => {
+            if (!thoughtsdata) {
+                res.status(404).json({ message: 'no thoughts found!'});
+                return;
+            }
+            res.json(thoughtsdata);
+        })
+        .catch(err => res.json(err));
+    },
+    // update thought
+    updateThought(req, res){
+        Thoughts.findByIdAndUpdate(
+            { _id: req.params.thoughtsdata}, { $set: req.body }, { runValidators: true, new: true})
+            .then(thoughtsdata => {
+                if (!thoughtsdata) {
+                    res.status(404).json({ message: 'no thoughts found!'});
+                    return;
+                }
+                res.json(thoughtsdata);
+            })
+            .catch(err => res.json(err));
+    },
+    // delating thought
+    deleteUser(req, res) {
+        Thoughts.findByIdAndRemove(
+            { _id: req.params.thoughtsdata}
+        )
+        .then(thoughtsdata => {
+            if (!thoughtsdata) {
+                res.status(404).json({ message: 'no thoughts found!'});
+                return;
+            }
+            res.json(thoughtsdata);
+        })
+        .catch(err => res.json(err));
+    }
 }
 
 module.exports = thoughtsController;
